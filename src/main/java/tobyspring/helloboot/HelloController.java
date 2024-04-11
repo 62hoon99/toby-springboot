7169b3ac4402d1e44f49d6214c338a0765007617
62hoon99 <ske06008@gmail.com>
2024-04-11 21:30:24 +0900

    section 4

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,19 +1,21 @@
 package tobyspring.helloboot;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(String name) {
-        return "Hello " + name;
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
     }
 
-    @GetMapping("/hello/json")
-    public ResponseEntity<String> helloJson(String name) {
-        return ResponseEntity.ok("Hello " + name);
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
