9fbf3ffb6fb869dbea5e6be014d75847ade03ae2
62hoon99 <ske06008@gmail.com>
2024-04-15 22:52:25 +0900

    feat: meta annotation 활용

---
+++ src/main/java/tobyspring/helloboot/Config.java
@@ -0,0 +1,22 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration // meta annotation 에 component 를 갖고 있음
+public class Config {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+}
