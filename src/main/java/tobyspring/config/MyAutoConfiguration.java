10e3b0321f389b43679db1f4f102218b65566cfe
dev_John <ske06008@gmail.com>
2024-04-18 13:54:31 +0900

    feat: proxyBeanMethods 학습 테스트 작성

--- src/main/java/tobyspring/config/MyAutoConfiguration.java
+++ src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false) // false 로 바꾸면
 public @interface MyAutoConfiguration {
 }
